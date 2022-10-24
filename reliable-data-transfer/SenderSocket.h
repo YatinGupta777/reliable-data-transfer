ab8439ddca71a7e9bc4a0172c42ba12873605b99
Yatin Gupta <yatingupta7777@gmail.com>
2022-10-24 16:26:47 -0500

    intial setup

---
+++ reliable-data-transfer/SenderSocket.h
@@ -0,0 +1,7 @@
+#pragma once
+// SenderSocket.h
+#define MAGIC_PORT 22345 // receiver listens on this port
+#define MAX_PKT_SIZE (1500-28) // maximum UDP packet size accepted by receiver
+class SenderSocket {
+	
+};
--- reliable-data-transfer/reliable-data-transfer.cpp
+++ reliable-data-transfer/reliable-data-transfer.cpp
@@ -12,8 +12,40 @@
 
 #include "pch.h"
 #include <iostream>
+#include "SenderSocket.h" 
 
-int main()
+int main(int argc, char** argv)
 {
     std::cout << "Hello World!\n";
+    if (argc != 8)
+    {
+        printf("Invalid Usage: Required [Lookup hostname/IP] [DNS server IP]");
+        return 0;
+    }
+
+    // parse command-line parameters
+    char* targetHost = argv[1];
+    int power = atoi(argv[2]); // command-line specified integer
+    int senderWindow = atoi(argv[3]); // command-line specified integer
+    UINT64 dwordBufSize = (UINT64)1 << power;
+    DWORD* dwordBuf = new DWORD[dwordBufSize]; // user-requested buffer
+    for (UINT64 i = 0; i < dwordBufSize; i++) // required initialization
+        dwordBuf[i] = i;
+    SenderSocket ss; // instance of your class
+    if ((status = ss.Open(targetHost, MAGIC_PORT, senderWindow, ...)) != STATUS_OK)
+        // error handling: print status and quit
+        char* charBuf = (char*)dwordBuf; // this buffer goes into socket
+    UINT64 byteBufferSize = dwordBufSize << 2; // convert to bytes
+    UINT64 off = 0; // current position in buffer
+    while (off < byteBufferSize)
+    {
+        // decide the size of next chunk
+        int bytes = min(byteBufferSize - off, MAX_PKT_SIZE - sizeof(SenderDataHeader));
+        // send chunk into socket
+        if ((status = ss.Send(charBuf + off, bytes)) != STATUS_OK)
+            // error handing: print status and quit
+            off += bytes;
+    }
+    if ((status = ss.Close()) != STATUS_OK)
+        // error handing: print status and quit 
 }
